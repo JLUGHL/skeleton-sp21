04d2bc22959940dcf6667b1093ca0f7c428bb0fc
Sleeping <3345212842@qq.com>
2025-07-03 21:18:27 +0800

    lab2
    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,10 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    public void testSquarePrimes() {
+        IntList lst = IntList.of(0,1,2,3,5,7,11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0,1,4,9,25,49,121", lst.toString());
+        assertTrue(changed);
+    }
 }
